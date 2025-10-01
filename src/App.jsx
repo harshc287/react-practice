b1069f4e5495bfedcf04d5140c26d06a2d0c9e39
harshc287 <chouhanharsh332@gmail.com>
2025-10-02 00:41:05 +0530

    trabutePageTilakVerma

--- src/App.jsx
+++ src/App.jsx
@@ -10,9 +10,9 @@ function App() {
       <div className='mainDiv'>
         <h1 className='heading'>Tilak Bhai Ki Jai Ho</h1>
         <img src={tilakImg} alt="tilak" />
-        <p className='para'>Tilak Varma was born in Kukatpally neighbourhood of Hyderabad, Andhra Pradesh (now in Telangana) on 8 November 2002 into a Telugu famliy.[5] His father Namboori Nagaraju Varma hails from Medchal, Telangana and worked as an electrician, whereas his mother Namboori Gayatri Devi hails from Bhimavaram, Andhra Pradesh and is a housewife. He has an elder brother, Tarun Varma.[6]
-Varma developed an early interest in cricket and was spotted playing tennis ball cricket by coach Salam Bayash at the age of 11. Bayash trained him at the Legala Cricket Academy in Lingampally, Hyderabad providing transport for the journey of more than 40 kilometres (25 mi) each way until Varma's family moved closer to the academy.[7][8][9][10]
-Varma was educated at Crescent Model English School and Bharatiya Vidya Bhavan, Jubilee Hills, Hyderabad before attending Lepakshi Junior College in the city.[11][12]</p>
+        <p className='para'>Tilak Varma was born in Kukatpally neighbourhood of Hyderabad, Andhra Pradesh (now in Telangana) on 8 November 2002 into a Telugu famliy.[5] His father Namboori Nagaraju Varma hails from Medchal, Telangana and worked as an electrician, whereas his mother Namboori Gayatri Devi hails from Bhimavaram, Andhra Pradesh and is a housewife. He has1. Bayash trained him at the Legala Cricket Academy in Lingampally, Hyderabad providing transport for the journey of more than 40 kilometres (25 mi) each way until Varma's family moved closer to the academy.[7][8][9][10]
+lege in the city.[11][12]</p>
+    <button className='readBtn' onClick={()=> window.open("https://en.wikipedia.org/wiki/Tilak_Varma")}>Read More</button>
       </div>
     </>
   )
